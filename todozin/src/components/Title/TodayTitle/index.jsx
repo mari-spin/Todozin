e84f51e8e97d2cc18cbf142e672c2d8e7ad2d83e
Mari Spinola <mariana.spinola@alura.com.br>
2023-05-18 21:58:48 -0300

    fix: title style

--- todozin/src/components/Title/TodayTitle/index.jsx
+++ todozin/src/components/Title/TodayTitle/index.jsx
@@ -1,9 +1,11 @@
+import { SubTitleStyle, TitleStyle, WrapperTitle } from "./style"
+
 const TodayTitle = () => {
     return (
-        <div>
-            <h2>Todo Zin</h2>
-            <h1>Task Management</h1>
-        </div>
+        <WrapperTitle>
+            <TitleStyle>Todo Zin</TitleStyle>
+            <SubTitleStyle>Task Management</SubTitleStyle>
+        </WrapperTitle>
     )
 }
 
---
+++ todozin/src/components/Title/TodayTitle/style.js
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+
+
+
+export const WrapperTitle = styled.div`
+
+    max-width: 600px;
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`
+
+export const SubTitleStyle = styled.h2`
+    color: var(--green);
+
+    font-size: 4rem;
+`
+
+export const TitleStyle = styled.h1``
